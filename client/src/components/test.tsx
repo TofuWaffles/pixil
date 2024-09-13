f602a34752ca4a81fe251c2b22f546dcea8966d5
Wolfus20 <shawnpandev@gmail.com>
2024-09-13 18:30:56 +0800

    Set color scheme

--- client/src/components/test.tsx
+++ client/src/components/test.tsx
@@ -2,8 +2,8 @@ import { Button } from "antd";
 
 export default function Test() {
     return (
-        <div className='bg-black white p-4'>
-            <Button className="bg-pink-100 black p-4">Cannywhat</Button>
+        <div className='bg-rose-taupe p-4'>
+            <Button className="bg-orchid-pink black p-4">Cannywhat</Button>
         </div>
     );
 }
