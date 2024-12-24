f57ea021f531a28d635de3830a50436a37578dd5
Wolfus20 <shawnpandev@gmail.com>
2024-12-24 14:42:46 +0800

    Set up database tables and fix healthcheck

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,20 @@
-CREATE TABLE IF NOT EXISTS seallove (
-  test INT NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+  email VARCHAR(255) PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TYPE media_status AS ENUM ('active', 'archived', 'deleted');
+
+CREATE TABLE IF NOT EXISTS media (
+  id SERIAL PRIMARY KEY,
+  path VARCHAR(255) NOT NULL,
+  owner_email VARCHAR(255),
+  file_type VARCHAR(255),
+  status media_status NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tag (
+  media_id INT REFERENCES media (id),
+  name VARCHAR(255) NOT NULL
 );
