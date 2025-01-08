c26480c74dabbf00ad434879a4633809ce738021
Wolfus20 <shawnpandev@gmail.com>
2025-01-08 21:50:05 +0800

    Add timeout middleware

--- backend/handlers/middleware.go
+++ backend/handlers/middleware.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 	"time"
 )
@@ -33,6 +34,24 @@ func (e Env) Logging() Middleware {
 			defer func() {
 				e.Logger.Info("Finished serving", "path", r.URL.Path, "remote_addr", r.RemoteAddr, "finished_in", time.Since(start))
 			}()
+
+			f(w, r)
+		}
+	}
+}
+
+func (e Env) Timeout(timeout time.Duration) Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			ctx, cancel := context.WithTimeout(r.Context(), timeout)
+			defer func() {
+				cancel()
+				if ctx.Err() == context.DeadlineExceeded {
+					w.WriteHeader(http.StatusGatewayTimeout)
+					e.Logger.Error("Handler timed out", "path", r.URL.Path)
+				}
+			}()
+			r = r.WithContext(ctx)
 
 			f(w, r)
 		}
