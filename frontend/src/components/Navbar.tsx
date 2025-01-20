275e3b05bb0f10eef7db463f6a9eba58884fa168
Wolfus20 <shawnpandev@gmail.com>
2025-01-20 21:07:12 +0800

    Refactor drawer menu icons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,8 +16,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
 
 const drawerWidth = 240;
 
@@ -106,6 +105,59 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+// TODO: Maybe combine these props into one interface
+function DrawerItem({ name, icon, open }: { name: string, icon: React.ReactElement, open: boolean }) {
+  return (
+    <ListItem key={name} disablePadding sx={{ display: 'block' }}>
+      <ListItemButton
+        sx={[
+          {
+            minHeight: 48,
+            px: 2.5,
+          },
+          open
+            ? {
+              justifyContent: 'initial',
+            }
+            : {
+              justifyContent: 'center',
+            },
+        ]}
+      >
+        <ListItemIcon
+          sx={[
+            {
+              minWidth: 0,
+              justifyContent: 'center',
+            },
+            open
+              ? {
+                mr: 3,
+              }
+              : {
+                mr: 'auto',
+              },
+          ]}
+        >
+          {icon}
+        </ListItemIcon>
+        <ListItemText
+          primary={name}
+          sx={[
+            open
+              ? {
+                opacity: 1,
+              }
+              : {
+                opacity: 0,
+              },
+          ]}
+        />
+      </ListItemButton>
+    </ListItem>
+  )
+}
+
 export default function MainDrawer() {
   let theme = useTheme();
   theme = responsiveFontSizes(theme);
@@ -143,67 +195,17 @@ export default function MainDrawer() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <div className='bg-background-dark'>
-        <Drawer variant="permanent" open={open}>
-          <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-          </DrawerHeader>
-          <Divider />
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                <ListItemButton
-                  sx={[
-                    {
-                      minHeight: 48,
-                      px: 2.5,
-                    },
-                    open
-                      ? {
-                        justifyContent: 'initial',
-                      }
-                      : {
-                        justifyContent: 'center',
-                      },
-                  ]}
-                >
-                  <ListItemIcon
-                    sx={[
-                      {
-                        minWidth: 0,
-                        justifyContent: 'center',
-                      },
-                      open
-                        ? {
-                          mr: 3,
-                        }
-                        : {
-                          mr: 'auto',
-                        },
-                    ]}
-                  >
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={text}
-                    sx={[
-                      open
-                        ? {
-                          opacity: 1,
-                        }
-                        : {
-                          opacity: 0,
-                        },
-                    ]}
-                  />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-      </div>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <DrawerItem name={'Home'} icon={<HomeIcon></HomeIcon>} open={open}></DrawerItem>
+        </List>
+      </Drawer>
     </Box>
   );
 }
