1c8f9466ab6cd7f28dac7fc37de7654f58d054c0
Wolfus20 <shawnpandev@gmail.com>
2025-02-15 17:17:55 +0800

    Add input fields

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,19 @@
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
 import Grid2 from "@mui/material/Grid2";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
 import useTheme from "@mui/material/styles/useTheme";
-import TextField from "@mui/material/TextField";
+import React from "react";
 
 export default function Login() {
   const theme = useTheme();
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   return (
     <Grid2
@@ -17,16 +27,37 @@ export default function Login() {
         backgroundColor: theme.palette.background.paper
       }}
     >
-      <TextField id="email" label="Email Address" variant="filled" sx={{
-        width: "60%",
-        backgroundColor: theme.palette.primary.light,
-        margin: "10px"
-      }} />
-      <TextField id="password" label="Password" variant="filled" sx={{
-        width: "60%",
-        backgroundColor: theme.palette.primary.light,
-        margin: "10px"
-      }} />
+      <FormControl sx={{ m: 1, width: "60%", backgroundColor: theme.palette.primary.light }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-email">Email Address</InputLabel>
+        <OutlinedInput
+          id="email"
+          label="Email Address"
+          autoFocus={true}
+        />
+      </FormControl>
+      <FormControl sx={{ m: 1, width: "60%", backgroundColor: theme.palette.primary.light }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label={
+                  showPassword ? 'hide the password' : 'display the password'
+                }
+                onClick={handleClickShowPassword}
+                onMouseDown={(event) => event.preventDefault()}
+                onMouseUp={(event) => event.preventDefault()}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
     </Grid2>
   )
 }
