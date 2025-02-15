7bcf3e203ea2b3821535f53d8022fd1ab28340cf
Wolfus20 <shawnpandev@gmail.com>
2025-02-15 17:32:06 +0800

    Create input fields

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Box } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
-import FormHelperText from "@mui/material/FormHelperText";
 import Grid2 from "@mui/material/Grid2";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -16,48 +16,61 @@ export default function Login() {
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   return (
-    <Grid2
-      container
-      spacing={0}
-      direction="column"
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="100%"
       alignItems="center"
       justifyContent="center"
+      minHeight="100vh"
       sx={{
-        minHeight: '100vh',
         backgroundColor: theme.palette.background.paper
-      }}
-    >
-      <FormControl sx={{ m: 1, width: "60%", backgroundColor: theme.palette.primary.light }} variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-email">Email Address</InputLabel>
-        <OutlinedInput
-          id="email"
-          label="Email Address"
-          autoFocus={true}
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: "60%", backgroundColor: theme.palette.primary.light }} variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-password"
-          type={showPassword ? 'text' : 'password'}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label={
-                  showPassword ? 'hide the password' : 'display the password'
-                }
-                onClick={handleClickShowPassword}
-                onMouseDown={(event) => event.preventDefault()}
-                onMouseUp={(event) => event.preventDefault()}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label="Password"
-        />
-      </FormControl>
-    </Grid2>
+      }}>
+      <Grid2
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        width="60%"
+        sx={{
+          minHeight: '100vh',
+        }}
+      >
+        <FormControl sx={{ m: 2, width: "80%", backgroundColor: theme.palette.primary.light }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-email">Email Address</InputLabel>
+          <OutlinedInput
+            id="email"
+            label="Email Address"
+            autoFocus={true}
+            fullWidth={true}
+          />
+        </FormControl>
+        <FormControl sx={{ m: 2, width: "80%", backgroundColor: theme.palette.primary.light }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? 'hide the password' : 'display the password'
+                  }
+                  onClick={handleClickShowPassword}
+                  onMouseDown={(event) => event.preventDefault()}
+                  onMouseUp={(event) => event.preventDefault()}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            fullWidth={true}
+          />
+        </FormControl>
+      </Grid2>
+    </Box>
   )
 }
