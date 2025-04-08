cfa79ec4d1e85dd6259ae3940d9667e79265bebf
Shawn Pan <shawnpandev@gmail.com>
2025-04-08 17:59:02 +0800

    Fix banner sizing

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,9 +1,13 @@
-import BannerImage from "../assets/banner.png";
+import BannerImage from "../assets/banner-transparent.png";
 import Box from "@mui/material/Box";
 
 export default function Banner() {
   return (
     <Box
+      sx={{
+        width: "25vw",
+        m: 5,
+      }}
       color="inherit"
       aria-label="banner"
       component="img"
